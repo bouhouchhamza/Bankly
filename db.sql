8554ddf2fd5c74429bde031e04a9037df3372ee6
bouhouchhamza <bouhouchhamza52@gmail.com>
2025-12-03 14:32:40 +0100

    feat(db): add Customers and advisor tables

--- db.sql
+++ db.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE BANKLY_db;
 USE BANKLY_db;
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    full_name VARCHAR(250),
+    email VARCHAR(250) UNIQUE,
+    phone VARCHAR(250),
+    registration_date DATETIME
+);
+CREATE TABLE advisor(
+    advisor_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    full_name VARCHAR (250),
+    email VARCHAR(250) UNIQUE
+);
