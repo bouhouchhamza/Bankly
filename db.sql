7525251b2c5122e37ec094e9d8c0b10bb1055bac
bouhouchhamza <bouhouchhamza52@gmail.com>
2025-12-03 14:35:40 +0100

    feat(db): finalize full database schema (Customers, advisor, accounts, transactions)

--- db.sql
+++ db.sql
@@ -12,3 +12,21 @@ CREATE TABLE advisor(
     full_name VARCHAR (250),
     email VARCHAR(250) UNIQUE
 );
+CREATE TABLE accounts(
+    account_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    account_number VARCHAR (250),
+    balence DECIMAL(15,2) ,
+    account_type VARCHAR(250),
+    customerid INT,
+    advisorid INT ,
+    FOREIGN KEY (customerid) REFERENCES Customers(customer_id),
+    FOREIGN KEY (advisorid) REFERENCES advisor(advisor_id)
+);
+CREATE TABLE transactions(
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY  NOT NULL,
+    amount DECIMAL (12,2),
+    transaction_type VARCHAR(50),
+    transaction_date DATETIME,
+    accountid int ,
+    FOREIGN KEY (accountid) REFERENCES accounts(account_id)
+);
